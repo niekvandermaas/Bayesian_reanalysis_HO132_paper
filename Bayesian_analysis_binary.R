575a4258f219976a02a1a045fb6c342cf49b9ff4
niekvandermaas <58216209+niekvandermaas@users.noreply.github.com>
2023-05-04 14:38:37 +0200

    Update Bayesian_analysis_binary.R

--- Bayesian_analysis_binary.R
+++ Bayesian_analysis_binary.R
@@ -83,7 +83,7 @@ for (l in 1:nrow(data)){
   # Running the model
   model <- jags.model(file = "Bayesian_analysis_binary_outcome.txt", data_list[[l]], n.chains = 3, n.adapt= 1000)
   update(model, 1000); # Burning 1000 samples
-  mcmc_samples[[l]] <- coda.samples(model, params_to_monitor, n.iter=100000)
+  mcmc_samples[[l]] <- coda.samples(model, params_to_monitor, n.iter=50000)
   
   results[[l]] <- mcmc_samples[[l]][[1]]
 }
